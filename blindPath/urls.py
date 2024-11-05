492b30be9b1b879dd6f3f71fe96f67bc93ac3c6e
pan---xy <1125844684@qq.com>
2024-11-05 14:51:33 +0800

    创建项目

--- blindPath/urls.py
+++ blindPath/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # 添加 include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),  # 包含 myapp 的 URLs
 ]
