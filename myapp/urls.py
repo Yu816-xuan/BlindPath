33b28cd5b2cedfed96de41bdb980356b3d8dcf6e
pan---xy <1125844684@qq.com>
2024-11-07 10:59:39 +0800

    web更改

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('monitoring_statistics/', monitoring_statistics, name='monitoring_statistics'),
     path('map/', views.map_view, name='map_view'),
+    path('api/monitor/<int:monitor_id>/details/', views.monitor_details, name='monitor_details'),
+    path('api/monitor/<int:monitor_id>/alarms/', views.get_alarms_for_monitor, name='get_alarms_for_monitor'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Monitor, Alarm
-
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 def home(request):
     monitors = Monitor.objects.all()
     # 默认选择第一个监控区域（如果用户没有选择）
@@ -18,6 +19,20 @@ def home(request):
         'selected_monitor': monitor,
         'alarms': alarms,
     })
+
+def monitor_details(request, monitor_id):
+    monitor = Monitor.objects.get(id=monitor_id)
+    data = {
+        "id": monitor.id,
+        "location": monitor.location,
+    }
+    return JsonResponse(data)
+
+def get_alarms_for_monitor(request, monitor_id):
+    # Filter alarms by the selected monitor
+    alarms = Alarm.objects.filter(monitor_id=monitor_id).values('obstacle_type', 'alarm_time', 'is_processed')
+    alarms_data = list(alarms)
+    return JsonResponse({'alarms': alarms_data})
 
 def monitoring_statistics(request):
     # 实时数据展示和统计图表
